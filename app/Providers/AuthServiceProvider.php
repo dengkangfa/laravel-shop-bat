af1a57fed2c1c1b0e429fc464e7f42cba89352ab
dengkangfa <dengkangfa@outlook.com>
2018-06-30 11:10:35 +0000

    用户界面 - 订单详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\UserAddress;
+use App\Policies\OrderPolicy;
 use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -15,7 +16,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        UserAddress::class => UserAddressPolicy::class
+        UserAddress::class => UserAddressPolicy::class,
+        Order::class => OrderPolicy::class
     ];
 
     /**
